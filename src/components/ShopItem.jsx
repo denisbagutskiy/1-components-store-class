787818f570a0355f59f47e36c68e6dd71ebc39b2
Denis Bagutskiy <denisbagutskiy@gmail.com>
2020-09-03 15:48:26 +0300

    fix props init style

--- src/components/ShopItem.jsx
+++ src/components/ShopItem.jsx
@@ -11,18 +11,18 @@ export class ShopItem extends React.Component {
     }
 
     render() {
-        const { item } = this.props;
+        const {brand, title, description, descriptionFull, price, currency} = this.props.item;
 
         return <div className="shop-item">
-            <h2>{item.brand}</h2>
-            <h1>{item.title}</h1>
+            <h2>{brand}</h2>
+            <h1>{title}</h1>
             <div className="description">
-                <h3>{item.description}</h3>
-                <p>{item.descriptionFull}</p>
+                <h3>{description}</h3>
+                <p>{descriptionFull}</p>
             </div>
             <div className="divider"></div>
             <div className="shop-item__cart">
-                <CurrencyFormat value={item.price} displayType={'text'} thousandSeparator={true} prefix={item.currency} decimalScale={2} fixedDecimalScale={true} renderText={value => <div className="price">{value}</div>} />
+                <CurrencyFormat value={price} displayType={'text'} thousandSeparator={true} prefix={currency} decimalScale={2} fixedDecimalScale={true} renderText={value => <div className="price">{value}</div>} />
                 <button>Добавить в корзину</button>
             </div>
         </div>
